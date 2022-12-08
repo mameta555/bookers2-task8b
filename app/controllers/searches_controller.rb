66fa74d684303faaad0c6a9f5985021faf71e392
EC2 Default User <ec2-user@ip-172-31-15-158.ap-northeast-1.compute.internal>
2022-12-08 09:36:04 +0000

    [Finish]検索機能完成

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,34 @@
+class SearchesController < ApplicationController
+  def search
+    @model = params["model"]
+    @method = params["method"]
+    @content = params["content"]
+    @records = search_for(@model, @content, @method)
+  end
+
+  private
+
+    def search_for(model, content, method)
+      if model == 'user'
+        if method == 'perfect'
+          User.where(name: content)
+        elsif method == 'pertial'
+          User.where('name LIKE?', '%'+content+'%')
+        elsif method == 'forward'
+          User.where('name LIKE?', content+'%')
+        else
+          User.where('name LIKE?', '%'+content)
+        end
+      elsif model == 'book'
+        if method == 'perfect'
+          Book.where(title: content)
+        elsif method == 'pertial'
+          Book.where('title LIKE?', '%'+content+'%')
+        elsif method == 'forward'
+          Book.where('title LIKE?', content+'%')
+        else
+          Book.where('title LIKE?', '%'+content)
+        end
+      end
+    end
+end
